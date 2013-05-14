ae62867a6fd8b0f70e767e17649d80e6678b20ca
adatanasov <naso.003@gmail.com>
2013-05-15 00:05:44 +0300

    More unit tests for Mine
    More unit tests for Mine

--- BattleField.Tests/MineTests.cs
+++ BattleField.Tests/MineTests.cs
@@ -9,10 +9,37 @@ namespace BattleField.Tests
     public class MineTests
     {
         [TestMethod]
+        public void MineConstrustor_CreateNewMine()
+        {
+            Mine invalidMine = new Mine(2, 2);
+        }
+
+        [TestMethod]
         [ExpectedException(typeof(InvalidMineCoordinatesException))]
         public void MineConstrustor_MustThrowExceptionXIsNegative()
         {
             Mine invalidMine = new Mine(-2, 2);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidMineCoordinatesException))]
+        public void MineConstrustor_MustThrowExceptionYIsNegative()
+        {
+            Mine invalidMine = new Mine(5, -7);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidMineCoordinatesException))]
+        public void MineConstrustor_MustThrowExceptionYIsBiggerThan10()
+        {
+            Mine invalidMine = new Mine(5, 12);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidMineCoordinatesException))]
+        public void MineConstrustor_MustThrowExceptionXIsBiggerThan10()
+        {
+            Mine invalidMine = new Mine(15, 2);
         }
     }
 }
