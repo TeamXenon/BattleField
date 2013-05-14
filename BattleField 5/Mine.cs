950f80aea0c05b7f55a0b34631bad20df371e9ef
Gunslinga <gunslinga19@gmail.com>
2013-05-14 23:13:04 +0300

    Added usings and setters to Mine.cs

--- BattleField 5/Mine.cs
+++ BattleField 5/Mine.cs
@@ -1,15 +1,55 @@
-﻿namespace BattleField
+using System;
+using System.Collections.Generic;
+
+namespace BattleField
 {
     public class Mine
     {
+        private int x;
+        private int y;
+
+        public int X
+        {
+            get
+            {
+                return this.x;
+            }
+            set
+            {
+                if (value >= Engine.MinSize || value < Engine.MaxSize)
+                {
+                    this.x = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Coordinates must be between 0 and 10!");
+                }
+            }
+        }
+
+        public int Y
+        {
+            get
+            {
+                return this.y;
+            }
+            set
+            {
+                if (value >= Engine.MinSize || value < Engine.MaxSize)
+                {
+                    this.y = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Coordinates must be between 0 and 10!");
+                }
+            }
+        }
         public Mine(int x, int y)
         {
             this.X = x;
             this.Y = y;
         }
-
-        public int X { get; set; }
-
-        public int Y { get; set; }
     }
 }
+
